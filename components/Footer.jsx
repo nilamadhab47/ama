0c899e735e49371e43600dcbaac3faaad2048ff9
Nilamadhab Senapati <nilamadhabsenapati@Nilamadabhs-MacBook-Air.local>
2023-09-07 04:18:35 +0530

    footer bg change

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import React from "react";
-import {BsFacebook, BsInstagram, BsLinkedin, BsDiscord, BsTwitter} from "react-icons/bs"
+import {
+  BsFacebook,
+  BsInstagram,
+  BsLinkedin,
+  BsDiscord,
+  BsTwitter,
+} from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div>
+    <>
       <div className="footerDetails bg-footer-background bg-cover bg-no-repeat flex justify-between text-black pt-16 pb-16 pr-[191px] pl-[74px]">
         <div className="logo flex flex-col gap-12">
           <Image
@@ -14,16 +20,15 @@ const Footer = () => {
             height={84}
             className=" mix-blend-darken"
           />
-            <div className="social-media flex justify-center gap-6 items-center">
-          <BsFacebook className=" text-2xl text-logoBgColor hover:text-logoHoverColor" />
-          <BsInstagram className=" text-2xl text-logoBgColor hover:text-logoHoverColor"/>
-          <BsLinkedin className=" text-2xl text-logoBgColor hover:text-logoHoverColor"/>
-          <BsDiscord className=" text-2xl text-logoBgColor hover:text-logoHoverColor"/>
-          <BsTwitter className=" text-2xl text-logoBgColor hover:text-logoHoverColor"/>
-      
+          <div className="social-media flex justify-center gap-6 items-center">
+            <BsFacebook className=" text-2xl text-logoBgColor hover:text-logoHoverColor" />
+            <BsInstagram className=" text-2xl text-logoBgColor hover:text-logoHoverColor" />
+            <BsLinkedin className=" text-2xl text-logoBgColor hover:text-logoHoverColor" />
+            <BsDiscord className=" text-2xl text-logoBgColor hover:text-logoHoverColor" />
+            <BsTwitter className=" text-2xl text-logoBgColor hover:text-logoHoverColor" />
+          </div>
         </div>
-        </div>
-      
+
         <div className="sitemap">
           <h6 className="font-inter">SITEMAP</h6>
           <ul className="mt-4 font-[15px]">
@@ -43,14 +48,16 @@ const Footer = () => {
               <br />
               Alpharetta, GA 30005
             </li>
-            <li><u>info@armatura.us</u></li>
+            <li>
+              <u>info@armatura.us</u>
+            </li>
           </ul>
         </div>
       </div>
       <div className="footerSign bg-footerBg h-[111px] p-4 flex items-center pl-24 font-poppins text-[#a2a6b6]">
         &#169;2022 - Armatura US
       </div>
-    </div>
+    </>
   );
 };
 
