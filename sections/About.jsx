3a8793cfc9f3d3e893b96d117505d06313b542d1
Nilamadhab Senapati <nilamadhabsenapati@Nilamadabhs-MacBook-Air.local>
2023-09-06 18:08:19 +0530

    about

--- sections/About.jsx
+++ sections/About.jsx
@@ -8,18 +8,18 @@ import React from 'react'
 const About = () => {
   return (
     <div className=' bg-about-background bg-cover bg-no-repeat h-[1376px] pr-[102px] pl-[102px] pt-[154px]'>
-      <div className="about flex">
-        <div className="details w-1/2">
+      <div className="about flex items-center justify-between">
+        <div className="details w-[56%]">
             <SectionHeading text={"About"} />
             <Heading text={"What is Armatura"} headingStyle={"text-header mt-6 mb-6"}/>
-            <Paragraph text={"Armatura is the world’s leading provider of advanced multi-modal biometric hardware, software and platforms which are based on deep learning and artificial intelligence. Our touchless face, palm and fingerprint matching technology is used in numerous industries which rely on highly secure & accurate identity & access management (IAM)"}  paragraphStyle={"text-black "}/>
+            <Paragraph text={"Armatura is the world’s leading provider of advanced multi-modal biometric hardware, software and platforms which are based on deep learning and artificial intelligence. Our touchless face, palm and fingerprint matching technology is used in numerous industries which rely on highly secure & accurate identity & access management (IAM)"}  paragraphStyle={"text-black mb-4"}/>
             <Paragraph text={"Armatura’s global headquarters is located in Atlanta, GA USA. R&D is located in the U.S. and India. All manufacturing takes place in our 30,000 sq ft Thailand factory.We welcome the chance to partner with you."} paragraphStyle={"text-black "}/>
         </div>
         <div className="about-image">
             <Image src={"/images/about-img.png"} alt='bg' width={485} height={484}/>
         </div>
       </div>
-      <div className="feature flex">
+      <div className="feature flex mb-8">
         <div className="feature-img w-1/2 mr-4">
           <div className="upper flex gap-4 mb-4">
             <FeatureBox src={"/images/face.svg"} headText={"Face Recognition"} description={"Seamless Security : Elevating Access Through Face Recognition"}/>
